f394effe2370dfb94c3fbf5a06e5a504d8e27551
Kimhak Han <74129379+kimhak1689@users.noreply.github.com>
2022-06-24 02:35:53 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  lintOnSave:false
+  lintOnSave:false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
 })
